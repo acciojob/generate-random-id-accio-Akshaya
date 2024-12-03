1b85b571c0cb6b832ddcccf84de207848982b47c
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-12-02 23:11:21 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
   // write your code here
+	var res="";
+	var char_list ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	for(var i=0;i<l;i++){
+		res += char_list.CharAt(
+			Math.floor(
+			Math.random() * char_list.length
+				)
+		)
+	}
 }
 
 // Do not change the code below.
